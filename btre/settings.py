423c2a61d89f319d437a5155ca62050eb11008f2
rms-sth <37908014+rms-sth@users.noreply.github.com>
2019-01-04 01:51:51 +0545

    Final Project completed (not done with email part)

--- btre/settings.py
+++ btre/settings.py
@@ -143,3 +143,10 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     # 50: 'critical',
 }
+
+#email configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ramesrest@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
--- contacts/admin.py
+++ contacts/admin.py
@@ -4,11 +4,9 @@ from . models import Contact
 
 # Register your models here.
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+    list_display = ('id', 'name', 'listing', 'email', 'message')
     list_display_links = ('id', 'name')
     search_fields = ('name', 'listing', 'email')
     list_per_page = 25
-
-
 
 admin.site.register(Contact, ContactAdmin)
