3526ab9eda4edf3ecdfd6ee0158ccaf4782df21e
rms-sth <37908014+rms-sth@users.noreply.github.com>
2019-01-03 23:19:42 +0545

    Contact Form

---
+++ listings/choices.py
@@ -0,0 +1,35 @@
+bedroom_choices = {
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    '10':10
+}
+
+price_choices = {
+    '500000':'Rs. 5 lakh ',
+    '1000000':'Rs. 10 lakh',
+    '1500000':'Rs. 15 lakh',
+    '2000000':'Rs. 20 lakh',
+    '2500000':'Rs. 25 lakh',
+    '3000000':'Rs. 30 lakh',
+    '3500000':'Rs. 35 lakh',
+    '4000000':'Rs. 40 lakh',
+    '4500000':'Rs. 45 lakh',
+    '5000000':'Rs. 50 lakh+ '
+}
+
+state_choices = {
+    '1':'Province 1',
+    '2':'Province 2',
+    '3':'Province 3',
+    '4':'Province 4',
+    '5':'Province 5',
+    '6':'Province 6',
+    '7':'Province 7'
+}
